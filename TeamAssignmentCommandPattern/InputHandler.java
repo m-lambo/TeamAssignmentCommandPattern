9d06876512c9afe932c04cc825345e1dec5d0da3
EvanBryer <EvanBryer@gmail.com>
2020-03-23 01:00:01 -0400

    Fix naming error and hashmap setup

--- TeamAssignmentCommandPattern/InputHandler.java
+++ TeamAssignmentCommandPattern/InputHandler.java
@@ -4,27 +4,16 @@ import java.util.HashMap;
 import java.util.stream.Stream;
 public class InputHandler implements Command {
 
-	HashMap<String, Command> mapForCommands;
+	HashMap<String, Command> commands;
 	
 	public InputHandler (Document document) {
-		mapForCommands = (HashMap<String, Command>) Stream.of (new Object[][] {
-				{"load" , LoadCommand}
-				{"save", SaveCommand}
-				{"spell" , SpellCommand}
-				{"print" , PrintCommand}
-		} );
+		commands.put("load" , LoadCommand);
+		commands.put("save", SaveCommand);
+		commands.put("spell" , SpellCommand);
+		commands.put("print" , PrintCommand);
 	}
 	public void InputEntered (String str) {
 		// call execute method from the map's value index whose key is string from parameter somehow?
-		if (str.contains("load")) {
-		}
-		else if (str.contains("save")) {
-			
-		}
-		else if (str.contains("spell")) {
-			
-		}
-		else
-			
+		commands.get(str);
 	}
 }
