f15cc649537a0770b5668c81b87d5dece1d79bb9
EvanBryer <EvanBryer@gmail.com>
2020-03-23 01:17:50 -0400

    Hashmap initialized

--- TeamAssignmentCommandPattern/InputHandler.java
+++ TeamAssignmentCommandPattern/InputHandler.java
@@ -2,19 +2,22 @@ package TeamAssignmentCommandPattern;
 
 import java.util.HashMap;
 import java.util.stream.Stream;
-public class InputHandler implements Command {
+public class InputHandler {
 
 	HashMap<String, Command> commands;
 	
 	public InputHandler (Document document) {
-		commands.put("load" , LoadCommand);
-		commands.put("save", SaveCommand);
-		commands.put("spell" , SpellCommand);
-		commands.put("print" , PrintCommand);
+		commands = new HashMap<String, Command>();
+		commands.put("load" , new LoadCommand(document));
+		//commands.put("save", new SaveCommand(document));
+		//commands.put("spell" , new SpellCommand(document));
+		//commands.put("print" , new PrintCommand(document));
 	}
 	public void inputEntered (String str) {
 		// call execute method from the map's value index whose key is string from parameter somehow?
-		Command runComm = commands.get(str);
-		runComm.execute();
+		if(commands.containsKey(str)) {
+			Command runComm = commands.get(str);
+			runComm.execute();
+		}
 	}
 }
--- TeamAssignmentCommandPattern/LoadCommand.java
+++ TeamAssignmentCommandPattern/LoadCommand.java
@@ -4,7 +4,7 @@ public class LoadCommand implements Command {
 	public Document document;
 	
 	public LoadCommand (Document document) {
-		document = document;
+		this.document = document;
 	}
 	public void execute() {
 		document.load();
