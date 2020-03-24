aa144e14121a6b0b8ef6b4731e31e72d468279c2
cherrimi <cherrimico@gmail.com>
2020-03-23 22:45:55 -0400

    Commented and changed Document.java

--- TeamAssignmentCommandPattern/Document.java
+++ TeamAssignmentCommandPattern/Document.java
@@ -1,19 +1,42 @@
 package TeamAssignmentCommandPattern;
 
 public class Document {
+	String name;
 	
+	/** public Document(String name)
+	 * Sets name to given name and prints that it was created
+	 * @param name
+	 */
 	public Document(String name) {
+		this.name = name;
 		System.out.println(name + " is created.");
 	}
+	
+	/** public void load()
+	 * Prints that Document is loading
+	 */
+	
 	public void load() {
 		System.out.println("Document is loaded into view.");
 	}
+	
+	/** public void save()
+	 * Prints that document is saving
+	 */
 	public void save() {
 		System.out.println("Document is being saved.");
 	}
-	public void spellCheck() {
+	
+	/** public void spell
+	 *  Prints that Document is being spell-checked
+	 */
+	public void spell() {
 		System.out.println("Document is being checked for spelling errors");
 	}
+	
+	/** public void print()
+	 * Prints that Document is printing
+	 */
 	public void print() {
 		System.out.println("Document is printing.");
 	}
