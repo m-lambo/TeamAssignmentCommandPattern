a3b3cfc895ef2c53a38ac1c39245bb839b80be51
cherrimi <briana20rovelli@gmail.com>
2020-03-24 00:04:01 -0400

    Commented LoadCommand

--- TeamAssignmentCommandPattern/LoadCommand.java
+++ TeamAssignmentCommandPattern/LoadCommand.java
@@ -3,9 +3,17 @@ package TeamAssignmentCommandPattern;
 public class LoadCommand implements Command {
 	public Document document;
 	
+	/** public LoadCommand(Document document)
+	 * Sets document to given document
+	 * @param document
+	 */
 	public LoadCommand (Document document) {
 		this.document = document;
 	}
+	
+	/** public void execute()
+	 * Calls load command in Document
+	 */
 	public void execute() {
 		document.load();
 	}
